f0adfde0e805d5d7a9bc4c1aa0dbb1f3e14afb24
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-05-31 01:57:54 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static OnTaskDoneListener listener = System.out::println;
+    static OnTaskErrorListener errorListener = System.out::println;
+
+    public static void main(String args[]) {
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+
+    }
+}
